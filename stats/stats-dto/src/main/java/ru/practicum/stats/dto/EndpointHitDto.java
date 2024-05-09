2e93c1990a5e55a4402933187746455d43bfb80e
akarui-shi <mmar1yakucherova@yandex.ru>
2024-05-09 16:02:48 +0300

    fix controller

--- stats/stats-dto/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
@@ -1,13 +1,17 @@
 package ru.practicum.stats.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EndpointHitDto {
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
